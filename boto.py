1d7593d88472c00df472be2db75299acbdcfa0e0
Johnpaul Nwagwu <40552765+jpnwagwu@users.noreply.github.com>
2022-07-26 10:20:19 -0700

    Add files via upload

---
+++ boto.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[10]:
+
+
+#importing libraries
+import nltk
+import numpy as np
+import random
+import string
+import io
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+#import warnings
+#warnings.filterwarnings('ignore')
+
+
+# In[11]:
+
+
+#Load and read file
+file = open('test.txt','r')
+data = file.read()
+
+
+# In[12]:
+
+
+#Convert all words in corpus to lower case and convert to list of words or sentences
+data = data.lower()
+sentences = nltk.sent_tokenize(data)
+words = nltk.word_tokenize(data)
+
+
+# In[13]:
+
+
+sentences[:3]
+
+
+# In[14]:
+
+
+words[:3]
+
+
+# In[15]:
+
+
+#Extracting root words with meanings
+lemma = nltk.stem.WordNetLemmatizer()
+
+def LemmaTokens(tokens):
+    return [lemma.lemmatize(token) for token in tokens]
+
+#Remove noise and stop words
+remove_punct_dict = dict((ord(punct), None) for punct in string.punctuation)
+
+#Lemmatize and remove stop words and noise
+def LemmaNormalize(word):
+    return LemmaTokens(nltk.word_tokenize(word.lower().translate(remove_punct_dict)))
+
+
+# In[16]:
+
+
+#Greetings 
+input_greetings = ("hello", "greetings", "hi", "sup", "what's up", "hey", "Good Morning", "Good Afternoon", "Good Evening", "How are you" )
+output_greetings = ["hi", "hey", "hi there", "I'm glad you are talking to me"]
+
+def greeting(sentence):
+    for word in sentence.split():
+        if word.lower() in input_greetings:
+            return random.choice(output_greetings)
+
+
+# In[17]:
+
+
+def response(user_input):
+    boto_response = ''
+    sentences.append(user_input)
+    TfidVec = TfidfVectorizer(tokenizer=LemmaNormalize, stop_words='english')
+    tfidf = TfidVec.fit_transform(sentences)
+    vals = cosine_similarity(tfidf[-1], tfidf)
+    idx = vals.argsort()[0][-2]
+    flat = vals.flatten()
+    flat.sort()
+    req_tfidf = flat[-2]
+    if (req_tfidf==0):
+        boto_response = boto_response+"Could you rephrase? Remember I talk only about Vector"
+    else:
+        boto_response = boto_response+sentences[idx]
+        return boto_response
+    
+    
+
+
+# In[ ]:
+
+
+flag=True
+print("BOTO: My name is Boto. I will answer your queries about Vector. If you want to exit, type Goodbye")
+while (flag==True):
+    user_response = input()
+    user_response = user_response.lower()
+    if (user_response != 'goodbye'):
+        if (user_response=='thanks' or user_response =='thank you'):
+            flag=False
+            print("BOTO: You are welcome..")
+        else:
+            if greeting(user_response)!=None:
+                print("BOTO: "+greeting(user_response))
+            else:
+                print("BOTO: ", end="")
+                print(response(user_response))
+                sentences.remove(user_response)
+    else:
+        flag=False
+        print("BOTO: Bye! Have a great day ")
+
+
+# In[ ]:
+
+
+
+
